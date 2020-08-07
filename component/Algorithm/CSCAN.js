fd320ceb513713c4514d586e1533e32ff1580f65
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-08-07 21:25:54 +0700

    add comment

--- component/Algorithm/CSCAN.js
+++ component/Algorithm/CSCAN.js
@@ -20,14 +20,18 @@ const CSCAN = ({ dataSet }) => {
     ] = useState(0)
 
     useEffect(() => {
+        //inisialisasi variabel sama ngambil data set yang berisi head + track. Head selalu di index ke 0
         let finalDataSet = [...dataSet]
         let timeDeviation = {}
         let averageSeekLength = 0
 
+        //disini inisialisasi juga dan ngambil head dari data set index ke 0 menggunakan fungsi shift
         const headDataSet = finalDataSet.shift()
         const dataSetBelowHead = []
         const dataSetUpperHead = []
 
+        //perulangan disini untuk ngebagi 2 bagian ada array yang diatas nilanya lebih besar dari head 
+        //sama array yang dibawah head yang nilainya lebih kecil daripada head
         for (let i = 0; i < finalDataSet.length; i++) {
             if (parseInt(finalDataSet[i]) > parseInt(headDataSet)) {
                 dataSetUpperHead.push(finalDataSet[i])
@@ -36,19 +40,27 @@ const CSCAN = ({ dataSet }) => {
             }
         }
 
+        //terus susun deh urutannya kalo CSCAN itu dari kecil ke besar
         AscendingSort(dataSetUpperHead)
         AscendingSort(dataSetBelowHead)
 
+        //habis itu gabungin lagi
         finalDataSet = dataSetUpperHead.concat(dataSetBelowHead)
 
+        //masukin head nya
         finalDataSet.unshift(headDataSet)
 
+        //ngitung waktu antara track
         timeDeviation = TimeDeviation(finalDataSet)
 
+        //masukin string kosong biar di label yang pertama gaada nilanya 
+        //karena emang head 
         timeDeviation.timeDeviationString.unshift(' ')
 
+        //ngitung rata2 nya
         averageSeekLength = GetSumOfNumberFromArray(timeDeviation.timeDeviationNumber) / timeDeviation.timeDeviationNumber.length
 
+        //masukin ke state nya
         setFinalDataSet(finalDataSet)
         setTime(timeDeviation.timeDeviationString)
         setAverageSeekLength(averageSeekLength.toFixed(1))
